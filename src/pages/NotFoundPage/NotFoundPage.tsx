04126ae334f80313852944c13f2d2f7fd4189c62
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-11-25 20:01:26 +0100

    add notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,15 @@
 import styles from './NotFoundPage.module.scss';
+import sprite from '../../images/sprite.svg';
 
 const NotFoundPage = () => {
-  return <div className={styles.page}>NotFoundPage</div>;
+  return (
+    <div className={styles.page}>
+      <svg className={styles.icon}>
+        <use href={`${sprite}#icon-error`} />
+      </svg>
+      <p className={styles.text}>Page is not found</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
